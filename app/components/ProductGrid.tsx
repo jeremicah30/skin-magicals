a3e697d8556dc7cf78667104ad12eb680e8be196
Jeremicah Licup <jlicup@n-compass.online>
2023-05-22 18:29:44 +0800

    First Commit

---
+++ app/components/ProductGrid.tsx
@@ -0,0 +1,52 @@
+import {useState, useEffect} from 'react';
+import {useFetcher} from '@remix-run/react';
+
+import ProductCard from './ProductCard';
+
+export default function ProductGrid({collection, url}: any) {
+  const fetcher = useFetcher();
+
+  const [nextPage, setNextPage] = useState(
+    collection.products.pageInfo.hasNextPage,
+  );
+
+  const [endCursor, setEndCursor] = useState(
+    collection.products.pageInfo.endCursor,
+  );
+
+  const [products, setProducts] = useState(collection.products.nodes || []);
+
+  function fetchMoreProducts() {
+    fetcher.load(`${url}?index&cursor=${endCursor}`);
+  }
+
+  useEffect(() => {
+    if (!fetcher.data) return;
+    const {collection} = fetcher.data;
+
+    setProducts((prev: any) => [...prev, ...collection.products.nodes]);
+    setNextPage(collection.products.pageInfo.hasNextPage);
+    setEndCursor(collection.products.pageInfo.endCursor);
+  }, [fetcher.data]);
+
+  return (
+    <section className="w-full gap-4 md:gap-8 grid">
+      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {products.map((product: any) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      {nextPage && (
+        <div className="flex items-center justify-center mt-6">
+          <button
+            className="inline-block rounded font-medium text-center py-3 px-6 border w-full cursor-pointer"
+            disabled={fetcher.state !== 'idle'}
+            onClick={fetchMoreProducts}
+          >
+            {fetcher.state !== 'idle' ? 'Loading...' : 'Load more products'}
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
---
+++ app/components/ProductOptions.tsx
@@ -0,0 +1,50 @@
+import {Link, useLocation, useSearchParams} from '@remix-run/react';
+
+export default function ProductOptions({options}: any) {
+  const {pathname, search} = useLocation();
+  const [currentSearchParams] = useSearchParams();
+
+  const searchParams = currentSearchParams;
+
+  return (
+    <div className="grid gap-4 mb-6">
+      {options.map((option: any) => {
+        if (!option.values.length) {
+          return;
+        }
+        const currentOptionVal = searchParams.get(option.name);
+        return (
+          <div
+            key={option.name}
+            className="flex flex-col flex-wrap mb-4 gap-y-2 last:mb-0"
+          >
+            <h3 className="whitespace-pre-wrap max-w-prose font-bold text-lead min-w-[4rem]">
+              {option.name}
+            </h3>
+
+            <div className="flex flex-wrap items-baseline gap-4">
+              {option.values.map((value: any) => {
+                const linkParams = new URLSearchParams(search);
+                const isSelected = currentOptionVal === value;
+                linkParams.set(option.name, value);
+                return (
+                  <Link
+                    key={value}
+                    to={`${pathname}?${linkParams.toString()}`}
+                    preventScrollReset
+                    replace
+                    className={`leading-none py-1 border-b-[1.5px] cursor-pointer transition-all duration-200 ${
+                      isSelected ? 'border-gray-500' : 'border-neutral-50'
+                    }`}
+                  >
+                    {value}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
